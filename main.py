40b6182436802acb0ca0abeaffcdc116f120358e
A1Code605 <c5021753@hallam.shu.ac.uk>
2026-06-09 21:42:12 +0100

    Add formated match list with dates

--- main.py
+++ main.py
@@ -20,6 +20,16 @@ response = requests.get(url, headers=headers, verify=False)
 data = response.json()
 matches = data["matches"]
 
+months = {
+    '01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr',
+    '05': 'May', '06': 'Jun', '07': 'Jul', '08': 'Aug',
+    '09': 'Sep', '10': 'Oct', '11': 'Nov', '12': 'Dec'
+}
+
 for match in matches:
     if match["homeTeam"]["name"] is not None:
-        print(f'{match["homeTeam"]["name"]} vs {match["awayTeam"]["name"]}')
+        date =  match["utcDate"].split("T")[0]
+        parts = date.split("-")
+        day = parts[2]
+        month = months[parts[1]]
+        print(f'{day} {month} | {match["homeTeam"]["name"]} vs {match["awayTeam"]["name"]}')
