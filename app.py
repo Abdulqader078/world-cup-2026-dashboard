6fce598b224c9549c4e8517bfe5ec7b52a8c52e7
A1Code605 <c5021753@hallam.shu.ac.uk>
2026-06-13 16:01:11 +0100

    Add match times and fix column alignment

--- app.py
+++ app.py
@@ -44,6 +44,10 @@ def index():
             match_date = match["utcDate"].split("T")[0]
             date_parts = match["utcDate"].split("T")[0].split("-")
             formatted_date = f'{date_parts[2]} {months[date_parts[1]]}'
+            raw_time = match["utcDate"].split("T")[1].split("Z")[0][:5]
+            hour = int(raw_time.split(":")[0]) + 1
+            minute = raw_time.split(":")[1]
+            corrected_time = f'{hour:02d}:{minute}'
             fixtures.append({
                 "date": formatted_date,
                 "home": match["homeTeam"]["name"],
@@ -51,7 +55,8 @@ def index():
                 "status": "Upcoming" if match["status"] == "TIMED" else match["status"],
                 "home_score":match["score"]["fullTime"]["home"],
                 "away_score":match["score"]["fullTime"]["away"],
-                "is_today": match_date == today_str
+                "is_today": match_date == today_str,
+                "time": corrected_time
             })
 
     return render_template('index.html', fixtures=fixtures)
